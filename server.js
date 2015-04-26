b43d23efb3c9351ab99bd17d9c1885cb28e611f9
Christian Gastrell <cgastrell@gmail.com>
2015-04-26 14:49:09 -0300

    added clear route /clear and enableInfo

--- server.js
+++ server.js
@@ -10,7 +10,18 @@ var redisStore = require("redis-tile-store");
 
 var ignTiles = {
   urlTemplate: 'http://wms.ign.gob.ar/geoserver/gwc/service/tms/1.0.0',
-  cachepath: 'cache'
+  storage: s3storage({
+    accessKeyId: "AKIAILWW6I2PSK3AOG3A",
+    secretAccessKey: "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",
+    region: "us-west-2",
+    bucket: "express-tilecache"
+  }),
+  store: redisStore({
+    port: 6379,
+    host: "tile-cache-redis.vcmzdd.0001.usw2.cache.amazonaws.com"
+  }),
+  enableInfo: true,
+  clearRoute: "/clear"
 }
 
 app.use('/', tilecache(ignTiles));
